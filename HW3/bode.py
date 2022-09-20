8d17b8c00210827a5ce142622b7fb4c28533e3d3
unknown <kidusfasil@gmail.com>
2022-09-20 17:54:35 -0400

    HW4 solution

--- HW3/bode.py
+++ HW3/bode.py
@@ -1,2 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+poles = [-10, -1000]
+zeros = [0]
+omega=np.logspace(start=-2, stop=5, base=10)
+s=1j*omega
+numerator=1 if len(zeros)==0 else np.absolute(s-zeros[0])
+denominator= 1 if len(poles)==0 else  np.absolute(s-poles[0] )
+for i in range(1, len(zeros)):
+    numerator=numerator * np.absolute((s-zeros[i]))
+for k in range(1, len(poles)):
+    denominator= denominator * np.absolute((s-poles[k]))
+#print(numerator, denominator)
+
+H=numerator/denominator
+print(H)
+H=20*np.log10(H)
+plt.semilogx(omega, H)
+plt.show()
