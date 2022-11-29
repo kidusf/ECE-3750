775a93bbf58e80c526c99eb5a4b8f86cdbfa759e
unknown <kidusfasil@gmail.com>
2022-11-29 14:54:27 -0500

    averaging filters

--- Project/averaging_project.py
+++ Project/averaging_project.py
@@ -4,32 +4,24 @@ import math
 from scipy import optimize
 import matplotlib as mpl
 
-f_s=480
+f_s=1600
 e=math.e
 H1=[0]*512
-H1.extend([1/8]*8)
+H1.extend([1/26]*26)
 H1.extend([0]*512)
 ones=[1]*1032
-H2=[0]*513
-H2.extend([1/6]*6)
+H2=[0]*512
+H2.extend([1/27]*27)
 H2.extend([0]*513)
-H=np.convolve(H1, H2, "same")
+H=np.convolve(H1, H2, "full")
 zeroes=np.roots(H)
 #print(zeroes)
 H=np.fft.fft(H)
-print(len(H))
 omega=np.linspace(start=-np.pi, stop=np.pi, num=len(H))
 H=20*np.log10(np.abs(H))
-between=[(H[i], omega[i])  for i in range(len(H)) if omega[i]>=60*2*np.pi/f_s and omega[i]<=80*2*np.pi/f_s]
-max_y, max_x=max(between)
-fig, ax = plt.subplots(figsize=(3, 3))
-ax.plot(omega[len(H)//2:], H[0:len(H)//2])
-ax.annotate('max point between 60 and 80 Hz: ('+str(round(max_x, 4))+" radians, "+str(round(max_y,4))+" dB, " +str(round(f_s*max_x/(2*np.pi), 4))+" Hz)",
-            xy=(max_x, max_y), xycoords='data',
-            xytext=(max_x, max_y), textcoords='offset points',
-            arrowprops=dict(facecolor='black', shrink=0.05),
-            horizontalalignment='right', verticalalignment='top')
-plt.xlabel("frequency (radians)")
+f=(f_s*omega)/(2*np.pi)
+plt.plot(f[len(H)//2:], H[0:len(H)//2+1])
+plt.xlabel("frequency (Hz)")
 plt.ylabel("| H | (dB)")
 
 #plt.legend(["60 Hz", "80 Hz", "combined"])
@@ -41,7 +33,6 @@ x = [zero.real for zero in zeroes ]
 # extract imaginary part
 y = [zero.imag for zero in zeroes]
 
-print(zeroes)
 mpl.rcParams['axes.linewidth'] = 0.1
 # plot the complex numbers
 plt.scatter(x, y, facecolors='none', edgecolors='r')
